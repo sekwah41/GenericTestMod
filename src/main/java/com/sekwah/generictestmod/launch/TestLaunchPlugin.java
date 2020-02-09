9da1b88218995315d043a39cc501f626f162eb5a
Sekwah <sekwah41@gmail.com>
2020-02-09 23:15:24 +0000

    Forgot code

--- src/main/java/com/sekwah/generictestmod/launch/TestLaunchPlugin.java
+++ src/main/java/com/sekwah/generictestmod/launch/TestLaunchPlugin.java
@@ -1,4 +1,24 @@
 package com.sekwah.generictestmod.launch;
 
-public class TestLaunchPlugin {
+import cpw.mods.modlauncher.serviceapi.ILaunchPluginService;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.ClassNode;
+
+import java.util.EnumSet;
+
+public class TestLaunchPlugin implements ILaunchPluginService {
+    @Override
+    public String name() {
+        return "generictestlaunchplugin";
+    }
+
+    @Override
+    public EnumSet<Phase> handlesClass(Type classType, boolean isEmpty) {
+        return EnumSet.<Phase>noneOf(Phase.class);
+    }
+
+    @Override
+    public boolean processClass(Phase phase, ClassNode classNode, Type classType) {
+        return false;
+    }
 }
