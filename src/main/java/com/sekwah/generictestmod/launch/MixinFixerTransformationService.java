0aa7107a058fb0c8a59fcb5496bb9b60fa31a1e3
Sekwah <sekwah41@gmail.com>
2020-02-10 04:30:07 +0000

    Need to get packages loading properly

--- src/main/java/com/sekwah/generictestmod/launch/MixinFixerTransformationService.java
+++ src/main/java/com/sekwah/generictestmod/launch/MixinFixerTransformationService.java
@@ -3,7 +3,6 @@ package com.sekwah.generictestmod.launch;
 import com.google.common.collect.ImmutableList;
 import cpw.mods.modlauncher.LaunchPluginHandler;
 import cpw.mods.modlauncher.Launcher;
-import cpw.mods.modlauncher.ServiceLoaderStreamUtils;
 import cpw.mods.modlauncher.api.IEnvironment;
 import cpw.mods.modlauncher.api.ITransformationService;
 import cpw.mods.modlauncher.api.ITransformer;
@@ -12,12 +11,12 @@ import cpw.mods.modlauncher.serviceapi.ILaunchPluginService;
 import joptsimple.OptionSpecBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
 import org.spongepowered.asm.launch.MixinLaunchPlugin;
 import org.spongepowered.asm.launch.MixinTransformationService;
+import org.spongepowered.asm.launch.platform.MixinPlatformAgentMinecraftForge;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.*;
 import java.util.function.BiFunction;
@@ -29,6 +28,8 @@ public class MixinFixerTransformationService implements ITransformationService {
     private boolean shouldProcessMixins = true;
 
     private LaunchPluginHandler launchPlugins;
+
+    private Map<String, ILaunchPluginService> pluginList;
 
     private MixinTransformationService mixinTransformationService;
 
@@ -56,9 +57,11 @@ public class MixinFixerTransformationService implements ITransformationService {
         // Don't grab this before in case someone else does something stupid such as replacing the list.
         // If mixins are detected skip any other processing so that whatever other plugin that has registered it
         // can take over.
+        LOGGER.info("Looking to add mixins");
         try {
             launchPlugins = (LaunchPluginHandler) launchPluginsField.get(Launcher.INSTANCE);
             if(launchPlugins.get("mixin").isPresent()) {
+                LOGGER.info("Mixins already found");
                 shouldProcessMixins = false;
                 return;
             }
@@ -68,8 +71,10 @@ public class MixinFixerTransformationService implements ITransformationService {
         }
 
         try {
-            Map<String, ILaunchPluginService> pluginList = getPluginList();
-            pluginList.put("mixin", new MixinLaunchPlugin());
+            LOGGER.info("Attempting to add mixins");
+            pluginList = getPluginList();
+            //pluginList.put("mixin", new MixinLaunchPlugin());
+            addLaunchPlugin("mixin", new MixinLaunchPlugin());
 
             if(!otherServices.contains("mixin")) {
                 mixinTransformationService = new MixinTransformationService();
@@ -77,6 +82,19 @@ public class MixinFixerTransformationService implements ITransformationService {
             }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void addLaunchPlugin(String name, ILaunchPluginService launchPluginService) {
+        pluginList.put("mixin", new MixinLaunchPlugin());
+        HashMap<String,String> mod = new HashMap<>();
+        mod.put("name", name);
+        mod.put("type", "PLUGINSERVICE");
+        String fName = launchPluginService.getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
+        mod.put("file", fName.substring(fName.lastIndexOf("/")));
+        if (Launcher.INSTANCE!=null) {
+            final List<Map<String, String>> mods = Launcher.INSTANCE.environment().getProperty(IEnvironment.Keys.MODLIST.get()).orElseThrow(() -> new RuntimeException("The MODLIST isn't set, huh?"));
+            mods.add(mod);
         }
     }
 
